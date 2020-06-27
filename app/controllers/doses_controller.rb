24fd7792a744876e012ed6c3927fc06f24ff3f4a
Mateusz Abramczyk <mateusz.abramczyk95@wp.pl>
2020-06-27 16:37:33 +0200

    almost done

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,26 @@
+class DosesController < ApplicationController
+  before_action :set_cocktail
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail), notice: 'Dose was successfully created.'
+    else
+      render :new
+    end
+  end
+
+private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
